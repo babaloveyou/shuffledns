3d30d54afdde7ce0c5ed8d0975a2850c7d9a4c95
babaloveyou <babaloveyou@users.noreply.github.com>
2021-01-14 16:17:41 +0800

    Update process.go

--- pkg/massdns/process.go
+++ pkg/massdns/process.go
@@ -177,7 +177,7 @@ func (c *Client) filterWildcards(st *store.Store) error {
 		}
 
 	count++
-	gologger.Infof("Task : %d/%d", count,,step)	
+	gologger.Infof("Task : %d/%d", count,step)	
 	}
 
 	wildcardWg.Wait()
